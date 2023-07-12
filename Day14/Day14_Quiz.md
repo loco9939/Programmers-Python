91606484632f0c3eb3e6003a9caa3835c98d98b6
loco9939 <kls9939@naver.com>
2023-07-13 00:08:35 +0900

    solve: Day14_Yiju.py

---
+++ Day14/Day14_Quiz.md
@@ -0,0 +1,5 @@
+# 문제
+
+QR 코드
+
+https://school.programmers.co.kr/learn/courses/30/lessons/181903
---
+++ Day14/Yiju.py
@@ -0,0 +1,7 @@
+def solution(q, r, code):
+    answer = ''
+    
+    for i in range(len(code)):
+        if (i % q == r):
+            answer += code[i]
+    return answer
